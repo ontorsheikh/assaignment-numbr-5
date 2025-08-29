c075509d3060280ae6646c855c7cf0e955c5a15a
ontorsheikh <skontorsheikh1613@gmail.com>
2025-08-29 09:45:12 +0600

    added heart count system

--- java/script.js
+++ java/script.js
@@ -1,1 +1,16 @@
-console.log("hello I am java-script")
+
+// heart count system
+
+let heartCount = 0;
+
+const heartClick = document.getElementsByClassName('heart-click');
+
+for (const heart of heartClick) {
+  heart.addEventListener('click', function () {
+    heartCount += 1;
+    document.getElementById('heart-count').innerHTML = heartCount;
+  });
+}
+
+
+
